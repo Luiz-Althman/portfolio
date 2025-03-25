deb99a93caf24a06d36a31e79d502b97e52b9157
luiz <luizalthmandev@gmail.com>
2025-03-25 17:31:29 -0300

    Test: Feature stripe

--- src/app/(home)/(section-portfolio)/components/box-projects.tsx
+++ src/app/(home)/(section-portfolio)/components/box-projects.tsx
@@ -5,120 +5,23 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import bhw from '../../../../assets/bhw.png';
-import hefesto from '../../../../assets/hefesto.jpg';
-import storybook from '../../../../assets/storybook.png';
-import kube from '../../../../assets/4kube.png';
-import animalShelter from '../../../../assets/animal-shelter.jpeg';
-import spotify from '../../../../assets/spotify.jpeg';
-import codecraft from '../../../../assets/codecraft.png';
-import pizzashop from '../../../../assets/pizzashop.png';
-import beautyClinic from '../../../../assets/beauty-clinic.png';
+import axios from 'axios';
 
-const projects = [
-    {
-        id: 0,
-        title: 'BHW - Lamark',
-        description:
-            'E-commerce desenvolvido do zero para a empresa Lamark. Aplicação feita em mobile e desktop.',
-        flags: [
-            'Online',
-            'ReactJs',
-            'NextJs',
-            'Material UI',
-            'Styled-components',
-        ],
-        src: bhw,
-        alt: 'E-commerce Best Hunter Wine',
-        link: 'https://besthunterwine.com.br/',
-    },
-    {
-        id: 1,
-        title: 'Hefesto Forge',
-        description:
-            'Aplicativo web view para ortopedia 4.0 e venda de orteses 3D (scans feitos dentro do app), para desktop e mobile com integração nativa.',
-        flags: ['ReactJs', 'Tailwind CSS', 'Typescript', 'Yup'],
-        src: hefesto,
-        alt: 'Aplicativo da Hefesto Medtech',
-    },
-    {
-        id: 2,
-        title: 'Design System',
-        description:
-            'Design system publicado no NPM. Projeto feito do zero dentro do curso da Rocketseat, com hospedagem no github pages.',
-        flags: ['Online', 'ReactJs', 'Stitches', 'Typescript'],
-        src: storybook,
-        alt: 'Storybook pessoal',
-        link: 'https://luiz-althman.github.io/design-system/?path=/docs/home--docs',
-    },
-    {
-        id: 3,
-        title: 'Codecraft - Rockeatseat',
-        description:
-            'Aplicação onde o usuário pode retirar seu ingresso e criar um link de convite. Onde possui um ranking de quais usuários mais tiveram mais convites.',
-        flags: ['ReactJs', 'NextJs v15', 'Tailwind v4', 'Typescript'],
-        src: codecraft,
-        alt: 'Plataforma de invites',
-    },
-    {
-        id: 4,
-        title: 'Pizza shop',
-        description:
-            'Sistema de backoffice para gerenciamento de restaurantes com testes unitários. Onde possui um ranking de quais usuários mais tiveram mais convites.',
-        flags: ['ReactJs', 'ViteJs', 'ShadCN UI', 'Tailwind CSS'],
-        src: pizzashop,
-        alt: 'Plataforma de gestão de restaurantes.',
-    },
-    {
-        id: 5,
-        title: '4Kube',
-        description:
-            'Site feito para uma empresa de DevOps, onde eles conseguiam divulgar seus serviços. Integrado com Typeform.',
-        flags: ['Online', 'ReactJs', 'NextJs v13', 'Bootstrap'],
-        src: kube,
-        alt: 'Site institucional para exibição da 4kube',
-        link: 'https://4kube.com',
-    },
-    {
-        id: 6,
-        title: 'Abrigo de animais',
-        description:
-            'Site institucional de um abrigo de animais. Feito apenas para desktop e hospedado na Railway.',
-        flags: ['Online', 'ReactJs', 'Vite', 'Tailwind v4', 'Railway'],
-        src: animalShelter,
-        alt: 'Site institucional de abrigo de animais',
-        link: 'https://animal-shelter.up.railway.app/',
-    },
-    {
-        id: 7,
-        title: 'Spotify Clone',
-        description:
-            'Uma aplicação clone do Spotify e com integração do next/intl para tradução (en/pt-br). Feito apenas para desktop.',
-        flags: ['Online', 'ReactJs', 'Nextjs v15', 'Tailwind v4', 'Next/Intl'],
-        src: spotify,
-        alt: 'Aplicação clone do Spotify',
-        link: 'https://spotify-web-devluiz.vercel.app',
-    },
-    {
-        id: 8,
-        title: 'Clínica de estética',
-        description:
-            'Site institucional feito para uma clínica de estética. Desenvolvido com NextJs v15 e TailwindCSS. Site responsivo e muito otimizado e com motion-react para animações.',
-        flags: [
-            'Online',
-            'ReactJs',
-            'Nextjs v15',
-            'Tailwind v4',
-            'Motion React',
-        ],
-        src: beautyClinic,
-        alt: 'Site institucional para clínica de estética',
-        link: 'https://beauty-clinic-spa.vercel.app',
-    },
-];
+type BoxProjectsProps = {
+    id: number;
+    name: string;
+    description: string;
+    flags: string[];
+    image: string;
+    url: string;
+    alt: string;
+};
 
 export function BoxProjects() {
     const [isVisible, setIsVisible] = useState(false);
+    const [projectsStripe, setProjectsStripe] = useState<BoxProjectsProps[]>(
+        []
+    );
 
     useEffect(() => {
         const handleScroll = () => {
@@ -135,12 +38,42 @@ export function BoxProjects() {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    useEffect(() => {
+        async function handleProjects() {
+            try {
+                const baseUrl =
+                    process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+
+                const response = await axios.get(`${baseUrl}/api/projects`);
+
+                const updatedProjects = response.data.map(
+                    (item: BoxProjectsProps) => {
+                        return {
+                            ...item,
+                            flags:
+                                typeof item.flags === 'string'
+                                    ? JSON.parse(item.flags)
+                                    : item.flags,
+                        };
+                    }
+                );
+
+                setProjectsStripe(updatedProjects);
+                console.log('data =>', updatedProjects);
+            } catch (err) {
+                console.error('Erro ao buscar projetos:', err);
+            }
+        }
+
+        handleProjects();
+    }, []);
+
     return (
         <div
             id="projects-section"
             className="flex flex-col md:grid md:grid-cols-3 gap-12"
         >
-            {projects.map((item, index) => (
+            {projectsStripe.map((item, index) => (
                 <motion.div
                     key={item.id}
                     initial={{ opacity: 0, y: 50 }}
@@ -156,12 +89,12 @@ export function BoxProjects() {
                 >
                     <div className="flex justify-between">
                         <h3 className="text-xl font-bold text-white pb-2">
-                            {item.title}
+                            {item.name}
                         </h3>
                         {item.flags[0] === 'Online' && (
                             <div className="flex justify-end">
                                 <Link
-                                    href={item.link || ''}
+                                    href={item.url || ''}
                                     target="_blank"
                                     className="text-purple-100 hover:text-purple transition-colors duration-300 font-bold"
                                 >
@@ -190,13 +123,14 @@ export function BoxProjects() {
                         ))}
                     </div>
                     <Image
-                        src={item.src}
+                        src={item.image}
                         alt={item.alt}
                         width={300}
                         height={200}
                     />
                 </motion.div>
             ))}
+             
         </div>
     );
 }
---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2025-02-24.acacia', // Sempre use a versão mais recente do Stripe
+});
+
+export async function GET() {
+    try {
+        const products = await stripe.products.list();
+
+        const projects = await Promise.all(
+            products.data.map(async (product) => {
+                const prices = await stripe.prices.list({
+                    product: product.id,
+                });
+
+                return {
+                    id: product.id,
+                    name: product.name,
+                    description: product.description,
+                    image: product.images[0],
+                    flags: product.metadata.flags || [],
+                    alt: product.metadata.alt || '',
+                    url: product.metadata.url || '#', // Busca a URL nos metadata
+                    price: prices.data[0]?.unit_amount || 0,
+                };
+            })
+        );
+
+        return NextResponse.json(projects);
+    } catch (error) {
+        return NextResponse.json(
+            { error: 'Erro ao buscar projetos' },
+            { status: 500 }
+        );
+    }
+}
